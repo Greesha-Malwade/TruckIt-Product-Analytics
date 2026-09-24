a2c5ba7dcdcaa101ddaaf0403dddd93351b6da51
Greesha Malwade <greeshamalwade@gmail.com>
2026-09-24 21:55:44 +0530

    Add Google Analytics tracking

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { DM_Sans, Instrument_Serif, Dancing_Script } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script";
 
 const dmSans = DM_Sans({
   variable: "--font-dm-sans",
@@ -29,10 +30,25 @@ export default function RootLayout({ children }) {
       lang="en"
       className={`${dmSans.variable} ${instrumentSerif.variable} ${dancingScript.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col font-sans" suppressHydrationWarning>
-        <Toaster position="top-center" />
-        {children}
-      </body>
+     <body className="min-h-full flex flex-col font-sans" suppressHydrationWarning>
+
+    <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-Q1NWXF5F99"
+        strategy="afterInteractive"
+    />
+
+    <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Q1NWXF5F99');
+        `}
+    </Script>
+
+    <Toaster position="top-center" />
+    {children}
+</body>
     </html>
   );
 }
